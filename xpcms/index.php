78d327e9af932625196e2ce25eae2b790680997f
nexd <nexd>
2006-01-02 19:17:25 +0000

    *** empty log message ***

--- xpcms/index.php
+++ xpcms/index.php
@@ -1,4 +1,7 @@
 <?php
+list($t1, $t2) = explode(' ', microtime());
+$start = $t1 + $t2;
+
 
 ini_set('include_path', 
 	ini_get('include_path') . 
@@ -7,4 +10,7 @@ ini_set('include_path',
 
 require_once 'prado/framework/prado.php';
 pradoGetApplication('XpCms/application.spec')->run();
+
+list($t1, $t2) = explode(' ', microtime());
+printf('Total time: %2.4f', (($t1 + $t2) - $start));
 ?>
