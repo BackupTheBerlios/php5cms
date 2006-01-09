d776d2a4db513676b5439c04da74b32c9b4865af
nexd <nexd>
2006-01-09 06:05:31 +0000

    Refactored CollectionUtil class which now uses a struct to return collection url info instead of parameters by reference.

--- xpcms/XpCms/Util/CollectionUtil.php
+++ xpcms/XpCms/Util/CollectionUtil.php
@@ -8,13 +8,11 @@
 class CollectionUtil {
     
     
-    public static function getPageClassAndAliasPath(WebCollection $collection,
-                                                                  &$pageClass,
-                                                                  &$aliasPath) {
+    public static function getPageClassAndAliasPath(WebCollection $collection) {
         
-        $lang = $collection->getWebPage()->Language;
+        $lang      = $collection->getWebPage()->Language;
+        $parts     = array($collection->getAlias());
         $pageClass = $collection->getPageClass();
-        $parts = array($collection->getAlias());
         
         $coll1 = $collection;
         $coll2 = $coll1->getParentCollection();
@@ -31,7 +29,46 @@ class CollectionUtil {
         }
         
         $aliasPath = implode('/', array_reverse($parts)) . '.' . $lang . '.html';
+        
+        return new CollectionInfo($pageClass, $aliasPath);
     }
     
+}
+
+/**
+ * This structure is used to return the alias path and an associated prado
+ * <code>TPage</code>-class for a <code>WebCollection</code>.
+ * 
+ * @package XpCms.Util
+ * @author Manuel Pichler <manuel.pichler@xplib.de>
+ * @version $Revision: 1.2 $  
+ */
+class CollectionInfo {
+    
+    /**
+     * The name of the <code>TPage</code>-class.
+     * 
+     * @var string $pageClass 
+     */
+    public $pageClass = null;
+    
+    /**
+     * The alias path for the <code>WebCollection</code>.
+     * 
+     * @var string $aliasPath
+     */
+    public $aliasPath = null;
+    
+    /**
+     * Simple constructor that takes the <code>TPage</code>-class name and the
+     * alias path as argument. Both parameters can also be <code>null</code>.
+     * 
+     * @param string $pageClass The name of the <code>TPage</code>-class.
+     * @param string $aliasPath The path for the <code>WebCollection</code>.
+     */
+    public function __construct($pageClass = null, $aliasPath = null) {
+        $this->pageClass = $pageClass;
+        $this->aliasPath = $aliasPath;
+    }
 }
 ?>
