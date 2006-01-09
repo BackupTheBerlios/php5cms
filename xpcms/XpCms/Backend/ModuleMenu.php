f26a0d50d4772d8034fe8e9d78df7151f257c0a6
nexd <nexd>
2006-01-09 06:04:07 +0000

    OSX like notebook look.

--- xpcms/XpCms/Backend/ModuleMenu.php
+++ xpcms/XpCms/Backend/ModuleMenu.php
@@ -21,17 +21,44 @@ class ModuleMenu extends TControl {
     public function ModuleMenu_OnItemCreated($sender, $param) {
         $category = $param->item;
         
-        CollectionUtil::getPageClassAndAliasPath($category->Data, $pageClass, $aliasPath);
+        $collectionInfo = CollectionUtil::getPageClassAndAliasPath(
+                                                            $category->Data);
         
-        $params['alias'] = $aliasPath;
+        $params['alias'] = $collectionInfo->aliasPath;
         
-        $URL = $this->Request->constructUrl($pageClass, $params);
-
-        $link = $category->ProductLink;
+        $URL = $this->Request->constructUrl($collectionInfo->pageClass, $params);
+        
+        $page = $this->Request->getParameter(TRequest::PAGE_SERVICE);
+        
+        $link = $category->TabItem;
+        
+        #print_r($result);
+        
+        $webPage  = $category->Data->getWebPage();
+        $helpText = '';
+        
+        if ($this->compare($page, $collectionInfo->pageClass)) {
+            $link->CssClass = 'tabTopActive';
+            $helpText       = $webPage->Description;
+        } else {
+            $link->CssClass = 'tabTopInActive';
+        }
+        
         $link->setEncodeText(false);
-        $link->Text = '<span>'.$category->Data->getWebPage()->Name.'</span>';       
+        $link->Text = $webPage->Name;
         $link->NavigateUrl = $URL;
-        $link->setAttribute('title',$category->Data->getWebPage()->Description);
+        $link->setAttribute('title', $webPage->Description);
+        
+        if ($helpText != '') {
+            $this->Parent->MainContentHelp->Text = $helpText;
+        }
+    }
+    
+    public function compare($name1, $name2) {
+        if (preg_match('#\:([A-Z][a-z0-9_]+)#', $name1, $match)) {
+            return strpos(substr($name2, strpos($name2, ':') + 1), $match[1]) === 0;
+        }
+        return false;
     }
 }
 ?>
--- xpcms/XpCms/Backend/ModuleMenu.tpl
+++ xpcms/XpCms/Backend/ModuleMenu.tpl
@@ -1,7 +1,14 @@
-<div>
+<div style="margin:0px;padding:0px;">
+  <ul style="list-style:none;height:25px;">
   <com:TRepeater ID="BackendMenu" OnItemCreated="ModuleMenu_OnItemCreated">
     <prop:ItemTemplate>
-      [<com:THyperLink ID="ProductLink" />]
+      <li class="tabItem">
+        <com:THyperLink ID="TabItem" />
+      </li>
     </prop:ItemTemplate>
   </com:TRepeater>
+  </ul>
+  <div style="width:100%;height:8px;background-image:url(/media/gfx/notebook_border.png);">
+  
+  </div>
 </div>
